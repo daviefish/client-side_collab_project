a1d74bb332ed564d549f1677efa2d2ce52511963
ilya osovets <iosovets@gmail.com>
2016-05-12 09:29:34 -0700

    worked on delete function, it now works suckaa

--- js/todosController.js
+++ js/todosController.js
@@ -3,30 +3,61 @@ angular.module('TodosApp')
 
 TodosController.$inject = ['TodoFactory'];
 
+
+
 function TodosController(TodoFactory) {
   var self = this;
   self.allTodos = [];
   self.api = TodoFactory;
+  self.getTodos = getTodos;
 
 
-
-  self.api.getAllTodos()
+function getTodos() {
+  TodoFactory.getAllTodos()
     .success(function(data) {
-      console.log("Getting all todos: " + data.todos);
-      self.allTodos = data.todos;
+      // console.log(data);
+      // console.log("Getting all todos: " + data.todos);
+      self.allTodos = data;
     });
+  }
+  getTodos()
 
     self.todos = {};
-
     self.showTodo = showTodo;
 
+
+
     function showTodo(id) {
-      console.log("Show Todo: " + id);
+      console.log(id);
       self.api.getOneTodo(id)
           .success(function(todo) {
             self.todo = todo;
             console.log(todo);
           });
+        }
+
+   self.deleteTodo =function(data) {
+      var id = (data._id);
+      self.api.deleteOneTodo(id)
+          .success(function(todo) {
+           // self.todo = todo;
+            console.log(todo);
+      // var index = self.all.indexOf(todo);
+      //  self.all.splice(index, 1);
+          getTodos()
+         })
+        };
+
+
+    // function addTodo(id) {
+    //   console.log("add todo: " + id);
+    //   self.api.getOneTodo(id)
+    //       .success(function(todo) {
+    //         self.todo = todo;
+    //         console.log(todo);
+    //       });
+
+
+
     }
-}
 
--- js/todosFactory.js
+++ js/todosFactory.js
@@ -1,20 +1,36 @@
-angular.module('TodosApp')
-  .factory('TodoFactory', TodoFactory);
+angular.module('TodosApp') //requires the module
+  .factory('TodoFactory', TodoFactory); //declaring the file as a factory and calling it and naming it TodoFactory
 
-TodoFactory.$inject = ['$http'];
 
+TodoFactory.$inject = ['$http']; //$http is a service from angualr used to make ajax call
+//injects the $http service into the todofactory
+
+
+///creating a todofactory function
 function TodoFactory($http) {
-    var todosURL = 'http://localhost:3000/api/todos';
-    var todoURL = 'http://localhost:3000/api/todos/:id';
-    var todoFactory = {};
+    var todosURL    = 'http://localhost:3000/api/todos'; //setting the api url
+    var todoURL     = 'http://localhost:3000/api/todos/'; // setting the api url for the individual ids
+    var todoFactory = {}; //makinga blank object
 
-    todoFactory.getAllTodos = function() {
-      return $http.get(todosURL);
+    todoFactory.getAllTodos = function() { //creatinga function in the todofactory called getalltodos
+      return $http.get(todosURL); //using the $http to make a get request on the specified url
     };
 
     todoFactory.getOneTodo = function(id) {
       return $http.get(todoURL + id);
     };
+
+    todoFactory.deleteOneTodo = function(id) {
+      console.log(todoURL + id)
+      return $http.delete(todoURL + id);
+    };
+
+    // todoFactory.addOneTodo = function(id) {
+    //   return $http.post(todoURL + id);
+    // };
+
+
+
 
     return todoFactory;
 }
